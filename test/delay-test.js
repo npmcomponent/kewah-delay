3d2575a3143d8de2704abefb0d8349fb22121ada
Antoine Lehurt <keuwah@gmail.com>
2013-06-29 23:59:53 +0200

    Refactor tests

--- test/delay-test.js
+++ test/delay-test.js
@@ -3,11 +3,8 @@ var delay = require('../');
 
 describe('delay()', function() {
   it('should be called 1s later', function(done) {
-    var d = Date.now();
-
-    delay(1000)(function() {
-      var delta = Date.now() - d;
-      assert(delta >= 1000);
+    this.timeout(510);
+    delay(500)(function() {
       done();
     });
   });
